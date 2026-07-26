b75913e498b32635d0d755a6b5fafde61b13f494
Johannes Habel <EchterAlsFake@proton.me>
2026-07-26 21:38:14 +0200

    - switched to AGPL - release

--- porntrex_api/__init__.py
+++ porntrex_api/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["Client", "Video", "Model", "Channel", "consts"]
+__all__ = ["Client", "Video", "Model", "Channel", "consts", "DownloadConfigRAW"]
 
-from porntrex_api.api import Client, Video, Model, Channel
+from porntrex_api.api import Client, Video, Model, Channel, DownloadConfigRAW
 from porntrex_api.modules import consts
